1664004992918fcce14dbcbe0db93b1130deb9c3
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-09-26 12:05:57 -0400

    Through pagination:

--- demo_code/app.js
+++ demo_code/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const { Anime } = require('./db/models')
 const productionStudioRouter = require('./routes/productionStudio')
 
 app.use(express.json());
 
 app.use('/productionstudio', productionStudioRouter);
+
+app.get('/search', async (req, res) => {
+    const { page, size } = req.query;
+
+    const pagination = {};
+
+    if (page > 0 && size > 0) {
+        pagination.limit = size;
+        pagination.offset = size * (page - 1)
+    }
+
+    if (!page) pagination.offset = 0;
+    if (!size) pagination.limit = 5;
+
+    console.log(pagination)
+    const anime = await Anime.findAll({
+        ...pagination
+    })
+
+    res.json(anime)
+})
 
 
 
