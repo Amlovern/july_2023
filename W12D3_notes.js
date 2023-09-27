ccc545b09b9b493fe157be358cc1a1b17daca0e9
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-09-27 13:42:55 -0400

    End of W12D3 Lecture

---
+++ W12D3_notes.js
@@ -0,0 +1,18 @@
+/*
+
+Security
+
+CORS - Cross Origin Resource Sharing
+    Allows other apps to send requests to our API
+    Enforced by the browser
+    CORS policies are just middleware
+
+XSS attack - Cross Site Scripting
+    Being able to inject code into someone else's website
+
+CSRF attack
+    When someone creates their own requests and tricks another user into sending that request
+
+Pretty common for XSS and CSRF attacks to happen together
+
+*/
