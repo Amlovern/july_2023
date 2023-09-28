45154d587cd72b772a416a84b2230d66b565a298
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-09-28 13:22:28 -0400

    Through JWT

--- W12D4_notes.js
+++ W12D4_notes.js
@@ -20,6 +20,40 @@ SCOPES
         <model>.scope(<scopes>).<query method>
         Higher index scopes (in the array passed to .scope) have priority
 
+JWT - JSON Web Token
+
+This is how we handle user auth
+
+How can we keep data safe as we transport it across the web?
+    Encoding
+        Not very secure. Can be easily decoded.
+    Encrypting
+        More secure. Can't be decrypted unless they have the password/secret key
+        If a bad actor get the secret key, we are in trouble
+    Hash
+        Cannot be reverse engineered
+        Hashing is deterministic
+        There is a problem here:
+            There is a possibility the multiple strings could end up with same hash value
+            This is called a hash collision
+        We use "Salt" to solve this problem
+
+A JWT consists of 3 parts:
+    Header
+        Contains the type of token
+        Indicator for the algo the we use for the hash
+    Payload
+        The data - ONLY encoded
+        Can add claims - such as an expiration
+    Signature
+        Hash of the header, payload, and a secret key
+        Allows us to validate that our token hasn't been tampered with
+
+    JWT can be easily decoded using something like jwt.io
+
+    jwt.sign is used to create a token
+    jwt.verify is used to verify a token
+        verify takes in an optional 3rd arg: a callback function
 
 
 */
